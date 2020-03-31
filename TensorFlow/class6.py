bf4a122bd281154348e677cc6782c8ba26e009c7
atom <1045099492@qq.com>
2020-03-31 13:27:38 +0800

    删除了TensorFlow

--- TensorFlow/class6.py
+++
@@ -1,9 +0,0 @@
-import cv2
-# 均值滤波
-
-img = cv2.imread("D:/python_workplace/OpenCV/test.jpg", 1)
-img_blur = cv2.blur(img, (10, 10))
-cv2.imshow("img", img)
-cv2.imshow("img_blur", img_blur)
-cv2.waitKey()
-cv2.destroyAllWindows()
